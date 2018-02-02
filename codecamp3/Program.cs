670ecd6e2a10be4707789afa04cb17aa297a701e
vuongducanh <anhvuong730@gmail.com>
2018-02-02 23:32:51 +0700

    update code camp 2

---
+++ codecamp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codecamp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
