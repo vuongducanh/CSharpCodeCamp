e908db0423c3c53ff7a6a9c10e01977187790a44
vuongducanh <anhvuong730@gmail.com>
2018-02-05 09:26:58 +0700

    updater

--- codecamp3/Program.cs
+++ codecamp3/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace codecamp3
 {
     class Program
     {
-         public void bai1()
+         static void bai1()
         {
-            // string[] cities = { "ROME","ZURICH","AMSTERDAM","SAIGON", "LONDON","HANOI","CALIFORNIA", "PARIS" };
-            System.Console.WriteLine("asdfrgv");
+            string[] cities = { "ROME","ZURICH","AMSTERDAM","SAIGON", "LONDON","HANOI","CALIFORNIA", "PARIS" };
+            List<string> list = new List<string>(); 
+            foreach (var item in cities)
+            {
+                list.Add(item);
+            }
+            list.Sort();
+          var kq = from s in list orderby s.Length select s;
+          foreach (var item in kq)
+          {
+              System.Console.WriteLine(item);
+          }
         }
           static void Main(string[] args)
         {
