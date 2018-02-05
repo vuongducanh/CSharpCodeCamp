cfad316cf7b97a2f8841cc04a026a7cde5812aab
vuongducanh <anhvuong730@gmail.com>
2018-02-05 10:11:07 +0700

    updater codecamp3

--- codecamp3/Program.cs
+++ codecamp3/Program.cs
@@ -20,9 +20,34 @@ namespace codecamp3
               System.Console.WriteLine(item);
           }
         }
+        static bool bai2(int[] arr1 , int[] arr2)
+        {
+            var num = from s in arr1 let s1=s*s select s1;
+            var result = num.Except(arr2);
+            if(result.Count()==0 && arr1.Length ==arr2.Length)
+            {
+              return true;
+            }
+            return false;
+        }
+        static string bai3(string[] arr)
+        {
+            
+        }
           static void Main(string[] args)
         {
-           bai1();
+        //    bai1();
+         
+         int[] arr1 = new int[]{1,2,3,4};
+         int[] arr2 = new int[]{1,4,9,16};
+         bool kq = bai2(arr1,arr2);
+         System.Console.WriteLine(kq);
+
+        string[] arrstring = new string[]{"anh","em","ai","day"};
+        string kq2 = bai3(arrstring);
+        System.Console.WriteLine(kq2); 
+
+         
         }
     }
 }
